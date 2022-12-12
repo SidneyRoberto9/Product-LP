a9e8117f5780e6b3cc53edd92485e3968a494df5
Sidney Roberto <sidneyrpsilva@gmail.com>
2022-12-12 18:15:15 -0300

    ✨ feat: Update Nav content

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 
+import { navigationData } from '../utils/hero.data';
+
 export const Nav = () => {
-	return <div>Nav</div>;
+	return (
+		<ul className='flex gap-x-8'>
+			{navigationData.map((item, index) => (
+				<li key={index}>
+					<a href={item.href}>{item.name}</a>
+				</li>
+			))}
+		</ul>
+	);
 };
--- src/components/NavMobile.tsx
+++ src/components/NavMobile.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
 
+import { navigationData } from '../utils/hero.data';
+
 export const NavMobile = () => {
-	return <div>NavMobile</div>;
+	return (
+		<ul className='flex flex-col px-6 py-8'>
+			{navigationData.map((item, index) => (
+				<li key={index}>
+					<a href={item.href} className='text-white hover:text-black'>
+						{item.name}
+					</a>
+				</li>
+			))}
+		</ul>
+	);
 };
